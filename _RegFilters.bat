b0a6c3c7b0f6223460fc679dd584c3b486a22091
Koji Hashimoto <hashi@iwate-pu.ac.jp>
2021-10-06 17:47:48 +0900

    Runtime modules of the MidField System (development version)

---
+++ _RegFilters.bat
@@ -0,0 +1,6 @@
+cd /d %~dp0
+
+regsvr32.exe DsConnection.dll
+regsvr32.exe DsMisc.dll
+regsvr32.exe Mixer.dll
+regsvr32.exe StreamIo.dll
---
+++ _UnregFilters.bat
@@ -0,0 +1,6 @@
+cd /d %~dp0
+
+regsvr32 /u DsConnection.dll
+regsvr32 /u DsMisc.dll
+regsvr32 /u Mixer.dll
+regsvr32 /u StreamIo.dll
